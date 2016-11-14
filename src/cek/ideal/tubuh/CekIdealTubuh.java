55442779dcd1f9f733875b6198a3a2aff4b752b9
Mang Arblu <khilyaalakbar@gmail.com>
2016-11-14 19:08:19 +0700

    commit tubuh ideal

---
+++ src/cek/ideal/tubuh/CekIdealTubuh.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cek.ideal.tubuh;
+
+/**
+ *
+ * @author Mang Arblu
+ */
+public class CekIdealTubuh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
